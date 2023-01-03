6a0b342fb73f820bf251daedddd0cffb80efb7a7
PhillipGimmi <phillip.gimmi@gmail.com>
2023-01-03 23:53:32 +0200

    update

--- Components/MicrophonePrompt/index.jsx
+++ Components/MicrophonePrompt/index.jsx
@@ -3,71 +3,61 @@ import { View, Text, TouchableOpacity, Linking, Alert, Platform } from 'react-na
 import * as Permissions from 'expo-permissions';
 
 const requestMicrophonePermission = async () => {
-  let permission;
-  if (Platform.OS === 'ios') {
-    const { status } = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
-    permission = status;
-  } else {
-    try {
-      const { status } = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
-      permission = status;
-    } catch (err) {
-      console.warn(err);
-      permission = 'denied';
-    }
-  }
-  return permission;
+let permission;
+if (Platform.OS === 'ios') {
+const { status } = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+permission = status;
+} else {
+try {
+const { status } = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+permission = status;
+} catch (err) {
+console.warn(err);
+permission = 'denied';
+}
+}
+return permission;
 };
 
 const openSettings = () => {
-  try {
-    Linking.openSettings();
-  } catch (error) {
-    console.error("Couldn't open settings", error);
-  }
+try {
+Linking.openSettings();
+} catch (error) {
+console.error("Couldn't open settings", error);
+}
 };
 
 const MicrophonePrompt = () => {
-  const [isDenied, setIsDenied] = useState(null);
-  const [isBlocked, setIsBlocked] = useState(null);
-  const [hasPermission, setHasPermission] = useState(null);
+const [isDenied, setIsDenied] = useState(null);
+const [isBlocked, setIsBlocked] = useState(null);
+const [hasPermission, setHasPermission] = useState(null);
 
-  useEffect(() => {
-    (async () => {
-      const status = await requestMicrophonePermission();
-      setHasPermission(status === 'granted');
-      setIsDenied(status === 'denied');
-      setIsBlocked(status === 'blocked');
-    })();
-  }, []);
+useEffect(() => {
+  const requestPermission = async () => {
+    const { status } = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+    setHasPermission(status === 'granted');
+    setIsDenied(status === 'denied');
+    setIsBlocked(status === 'blocked');
+  };
+  requestPermission();
+}, []);
 
-  useEffect(() => {
-    const unsubscribe = Permissions.addListener(({ type, status }) => {
-      if (type === 'audioRecording') {
-        setHasPermission(status === 'granted');
-        setIsDenied(status === 'denied');
-        setIsBlocked(status === 'blocked');
-      }
-    });
-    return unsubscribe;
-  }, []);
-
-  if (hasPermission === null) {
-    return <Text style={{ textAlignVertical: "center", flex: 1, textAlign: "center" }}>Requesting microphone permission</Text>;
-  }
-  if (hasPermission === false && isBlocked === true) {
-    return (
-      <View style={{ backgroundColor: 'red' }}>
-        <Text>
-          You have blocked the microphone permission. Please go to your device's settings and allow
-          the app to access the microphone in order to use this app.
-        </Text>
-        <TouchableOpacity onPress={openSettings}>
-          <Text style={{ textAlignVertical: "center", flex: 1, textAlign: "center" }}>Open settings</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+if (hasPermission === null) {
+return <Text style={{ textAlignVertical: "center", flex: 1, textAlign: "center" }}>Requesting microphone permission</Text>;
+}
+if (hasPermission === false && isBlocked === true) {
+return (
+<View style={{ backgroundColor: 'red' }}>
+<Text>
+You have blocked the microphone permission. Please go to your device's settings and allow
+the app to access the microphone in order to use this app.
+</Text>
+<TouchableOpacity onPress={openSettings}>
+<Text style={{ textAlignVertical: "center", flex: 1, textAlign: "center" }}>Open settings</Text>
+</TouchableOpacity>
+</View>
+);
+}
   if (hasPermission === false && isDenied === true) {
     return (
       <View style={{ backgroundColor: 'red' }}>
