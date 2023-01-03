195ffe422a592b6e1035c4754364e1cac16272be
PhillipGimmi <104858994+PhillipGimmi@users.noreply.github.com>
2023-01-03 13:21:25 +0200

    Update closeApp.jsx

--- Components/CommandList/commands/closeApp.jsx
+++ Components/CommandList/commands/closeApp.jsx
@@ -1,58 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { View, AppState } from 'react-native';
-import Snowboy from 'react-native-snowboy';
-import * as Speech from 'expo-speech';
+import { Platform, BackHandler } from 'react-native';
 
 const CloseApp = () => {
-const [isListening, setIsListening] = useState(false);
-const [appState, setAppState] = useState(AppState.currentState);
-
-const startListening = () => {
-setIsListening(true);
-Speech.startAsync({
-onResult: (event) => {
-setIsListening(false);
-if (event.value[0].transcript === 'close app') {
-if (appState === 'active') {
-AppState.addEventListener('change', handleAppStateChange);
-AppState.changeAppState('background');
-}
-}
-},
-onError: (error) => {
-console.error(error);
-setIsListening(false);
-},
-});
+  if (Platform.OS === 'ios') {
+    // For iOS devices
+    Alert.alert('Cannot close this app', 'To close the app, please press the home button.', [{ text: 'OK' }]);
+  } else {
+    // For Android devices
+    BackHandler.exitApp();
+  }
 };
-
-const stopListening = () => {
-Speech.stop();
-setIsListening(false);
-};
-
-useEffect(() => {
-Snowboy.start('close app', (error) => {
-if (error) {
-console.error(error);
-} else {
-console.log('Snowboy started listening for "close app"');
-startListening();
-}
-});
-return () => {
-Snowboy.stop((error) => {
-if (error) {
-console.error(error);
-} else {
-console.log('Snowboy stopped listening');
-}
-});
-stopListening();
-};
-}, []);
-
-return <View />;
-};
-
-export default CloseApp;
+export default CloseApp
