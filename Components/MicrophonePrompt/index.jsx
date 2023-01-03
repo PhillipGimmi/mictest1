7daf3e31084bcc6360a86a55f8a99ebfcefd9fbe
PhillipGimmi <phillip.gimmi@gmail.com>
2023-01-03 23:02:22 +0200

    test

--- Components/MicrophonePrompt/index.jsx
+++ Components/MicrophonePrompt/index.jsx
@@ -1,31 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, Linking, Alert } from 'react-native';
-import { Platform } from 'react-native';
+import { View, Text, TouchableOpacity, Linking, Alert, Platform } from 'react-native';
 import * as Permissions from 'expo-permissions';
-import PermissionsAndroid from 'react-native-permissions';
 
 const requestMicrophonePermission = async () => {
   let permission;
   if (Platform.OS === 'ios') {
-    const { status } = await Permissions.request('microphone');
+    const { status } = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
     permission = status;
   } else {
     try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
-        {
-          title: 'Microphone Permission',
-          message: 'App needs access to your microphone to record audio.',
-          buttonNeutral: 'Ask Me Later',
-          buttonNegative: 'Cancel',
-          buttonPositive: 'OK',
-        },
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        permission = 'granted';
-      } else {
-        permission = 'denied';
-      }
+      const { status } = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+      permission = status;
     } catch (err) {
       console.warn(err);
       permission = 'denied';
@@ -58,7 +43,7 @@ const MicrophonePrompt = () => {
 
   useEffect(() => {
     const unsubscribe = Permissions.addListener(({ type, status }) => {
-      if (type === 'microphone') {
+      if (type === 'audioRecording') {
         setHasPermission(status === 'granted');
         setIsDenied(status === 'denied');
         setIsBlocked(status === 'blocked');
@@ -83,32 +68,25 @@ const MicrophonePrompt = () => {
       </View>
     );
   }
-
-if (hasPermission === null) {
-return <Text style={{ textAlignVertical: "center", flex: 1, textAlign: "center" }}>Requesting microphone permission</Text>;
-}
-if (hasPermission === false && isBlocked === true) {
-return (
-<View style={{ backgroundColor: 'red' }}>
-<Text>
-You have blocked the microphone permission. Please go to your device's settings and allow
-the app to access the microphone in order to use this app.
-</Text>
-<TouchableOpacity onPress={openSettings}>
-<Text style={{ textAlignVertical: "center", flex: 1, textAlign: "center" }}>Open settings</Text>
-</TouchableOpacity>
-</View>
-);
-}
-return (
-<View style={{ flex: 1 }}>
-<TouchableOpacity onPress={requestMicrophonePermission} style={{ backgroundColor: 'green' }}>
-<Text style={{ textAlignVertical: "center", flex: 1, textAlign: "center" }}>
-Toggle microphone permission
-</Text>
-</TouchableOpacity>
-</View>
-);
+  if (hasPermission === false && isDenied === true) {
+    return (
+      <View style={{ backgroundColor: 'red' }}>
+        <Text>
+          You have denied the microphone permission.
+        </Text>
+        <TouchableOpacity onPress={requestMicrophonePermission}>
+          <Text style={{ textAlignVertical: "center", flex: 1, textAlign: "center" }}>Request microphone permission</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+  return (
+    <View style={{ flex: 1 }}>
+      <TouchableOpacity onPress={requestMicrophonePermission} style={{ backgroundColor: 'green' }}>
+        <Text style={{ textAlignVertical: "center", flex: 1, textAlign: "center" }}>Grant microphone permission</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default MicrophonePrompt;
